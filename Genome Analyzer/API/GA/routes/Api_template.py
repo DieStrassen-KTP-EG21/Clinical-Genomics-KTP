9520ec39d44a882b11b77d156a5f02bcc213ef76
Abdelrhman Hany <mindenslavement@gmail.com>
2021-08-22 18:25:11 +0200

    Creating API structure

---
+++ Genome Analyzer/API/GA/routes/Api_template.py
@@ -0,0 +1,19 @@
+from flask_restful import Resource
+
+class Api_template(Resource):
+    # Write method which will be executed via GET request
+    def get(self):
+        return {'geet': 'get req'}, 200
+
+    # Write method which will be executed via POST request
+    def post(self):
+        return {'poost': 'post req'}, 200
+
+    # Write method which will be executed via PUT request
+    def put(self):
+        return {'puut': 'put req'}, 200
+
+     # Write method which will be executed via DELETE request
+    def delete(self):
+        return {'deel': 'del req'}, 200
+    
