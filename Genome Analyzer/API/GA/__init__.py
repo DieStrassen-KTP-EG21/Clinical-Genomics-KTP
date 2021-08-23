421537a6d0b76b91963ec49737cc470b83e94188
Etchbanna <albannahisham@gmail.com>
2021-08-23 11:45:47 +0200

    Added Absolute Imports

--- Genome Analyzer/API/GA/__init__.py
+++ Genome Analyzer/API/GA/__init__.py
@@ -1,19 +1,18 @@
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).parent.absolute()))
 from flask import Flask
 from flask_restful import Api, Resource, reqparse
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///GA_Database.db'
 api = Api(app)
-db = SQLAlchemy(app)
 
 
-#importing models
-from GA.models.staff import Staff
 
 
 # import APIs classes here
-from GA.routes.api_template import Api_template
+from routes.Api_template import Api_template
 
 
 
