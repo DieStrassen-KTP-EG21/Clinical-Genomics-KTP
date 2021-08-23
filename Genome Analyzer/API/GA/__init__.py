1be6d5958ee161d25767445e2865091c7e99b1dd
Etchbanna <albannahisham@gmail.com>
2021-08-23 02:21:53 +0200

    Database URI Created

--- Genome Analyzer/API/GA/__init__.py
+++ Genome Analyzer/API/GA/__init__.py
@@ -3,12 +3,13 @@ from flask_restful import Api, Resource, reqparse
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///GA_Database.db'
 api = Api(app)
 db = SQLAlchemy(app)
 
 
 # import APIs classes here
-from GA.routes.Api_template import Api_template
+from routes.Api_template import Api_template
 
 
 
