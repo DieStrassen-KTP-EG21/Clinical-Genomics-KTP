a5ea3d7c0d2451d56fb49a3abfc189d8607718f6
Etchbanna <albannahisham@gmail.com>
2021-08-25 21:54:14 +0200

    Fix genbank

--- Genome Analyzer/API/GA/models/genbank.py
+++ Genome Analyzer/API/GA/models/genbank.py
@@ -5,7 +5,6 @@ from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 
 AllRecs=[]
-<<<<<<< HEAD
 file_names=["alenquer.gbk","dabiebandavirus.gbk","ebolavirus.gbk","influenza-a.gbk","hiv.gbk","sars.gbk"]
 
 for file in file_names:
@@ -13,20 +12,6 @@ for file in file_names:
         AllRecs.append(sq)
 
 # sseq=Seq("CGTAGCTAGTGCTAGTCGTAGCAGTCGTGTCAGTCGTACGTATGCTGATGCTAGCGTACGTAGCTAGTGCTAGTCGTAGCAGTCGTGTCAGTCGTACGTATGCTGATGCTAGCGTACGTAGCTAGTGCTAGTCGTAGCAGTCGTGTCAGTCGTACGTATGCTGATGCTAGCGTACGTAGCTAGTGCTAGTCGTAGCAGTCGTGTCAGTCGTACGTATGCTGATGCTAGCGTACGTAGCTAGTGCTAGTCGTAGCAGTCGTGTCAGTCGTACGTATGCTGATGCTAGCGTACGTAGCTAGTGCTAGTCGTAGCAGTCGTGTCAGTCGTACGTATGCTGATGCTAGCGTACGTAGCTAGTGCTAGTCGTAGCAGTCGTGTCAGTCGTACGTATGCTGATGCTAGCGTACGTAGCTAGTGCTAGTCGTAGCAGTCGTGTCAGTCGTACGTATGCTGATGCTAGCGTACGTAGCTAGTGCTAGTCGTAGCAGTCGTGTCAGTCGTACGTATGCTGATGCTAGCGTACGTAGCTAGTGCTAGTCGTAGCAGTCGTGTCAGTCGTACGTATGCTGATGCTAGCGTACGTAGCTAGTGCTAGTCGTAGCAGTCGTGTCAGTCGTACGTATGCTGATGCTAGCGTACGTAGCTAGTGCTAGTCGTAGCAGTCGTGTCAGTCGTACGTATGCTGATGCTAGCGTACGTAGCTAGTGCTAGTCGTAGCAGTCGTGTCAGTCGTACGTATGCTGATGCTAGCGTACGTAGCTAGTGCTAGTCGTAGCAGTCGTGTCAGTCGTACGTATGCTGATGCTAGCGTACGTAGCTAGTGCTAGTCGTAGCAGTCGTGTCAGTCGTACGTATGCTGATGCTAGCGTACGTAGCTAGTGCTAGTCGTAGCAGTCGTGTCAGTCGTACGTATGCTGATGCTAGCGTA")
-=======
-file_names=["alenquer.gbk","dabiebandavirus.gbk","ebolavirus.gbk","influenza-a.gbk","hiv.gbk"]
-
-for file in file_names:
-    for sq in SeqIO.parse("disease_sequences/"+file,"genbank"):
-        AllRecs.append(sq)
-
-AllRecs[0].description="Alenquer virus"
-AllRecs[1].description="Severe fever with thrombocytopenia syndrome virus"
-AllRecs[2].description="Zaire ebolavirus"
-AllRecs[3].description="Influenza A virus"
-AllRecs[4].description="Human immunodeficiency virus 1 (HIV-1)"
-
->>>>>>> 819d0965117b7a8535906257a032041c4ef975df
 
 aligner = Align.PairwiseAligner()
 aligner.match_score=1.0
@@ -34,26 +19,19 @@ aligner.match_score=1.0
 
 def compare(patientDna,storedData,resultDisease):
     scores=[]
-<<<<<<< HEAD
     DiseaseScores={}
-=======
->>>>>>> 819d0965117b7a8535906257a032041c4ef975df
     lastm=None
     for rec in storedData:
         result=pairwise2.align.globalxx(patientDna,rec.seq)
         # print(rec.description)
         # print(format_alignment(*result[0], full_sequences=True))
-<<<<<<< HEAD
         DiseaseScores[rec.description]=str(aligner.score(patientDna,rec.seq))
-=======
->>>>>>> 819d0965117b7a8535906257a032041c4ef975df
         scores.append(aligner.score(patientDna,rec.seq))
         # print(aligner.score(patientDna,rec.seq))
         m=max(scores)
         if(lastm!=m):
             resultDisease=rec.description
         lastm=m
-<<<<<<< HEAD
     return{'ResultDisease':resultDisease, 'HighScore':max(scores), 'Allscores':scores,'AllDiseasesScores':DiseaseScores}
 
 # res=None
@@ -63,7 +41,5 @@ def compare(patientDna,storedData,resultDisease):
 # print(a['AllDiseasesScores'])
 
 
-=======
->>>>>>> 819d0965117b7a8535906257a032041c4ef975df
 
 
