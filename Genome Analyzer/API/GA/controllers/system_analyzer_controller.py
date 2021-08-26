5afa3b2301a24f66c38a185a0b107ce1dddc8c8a
Etchbanna <albannahisham@gmail.com>
2021-08-26 02:46:40 +0200

    JSONified

--- Genome Analyzer/API/GA/controllers/system_analyzer_controller.py
+++ Genome Analyzer/API/GA/controllers/system_analyzer_controller.py
@@ -1,3 +1,4 @@
+from flask import jsonify
 from GA import db, app
 from GA.models.genbank import file_names, AllRecs, compare
 from GA.models.patient import Patient
@@ -8,8 +9,8 @@ def analyze_this(patientID,storedData):
     patient= Patient.query.get(patientID)
     if(patient):
         Result=compare(patient.Sequence, storedData)
-        return{'success':True,'message':"Comparison Done Successfully","Result Disease ": Result['ResultDisease'],
-        "Result Disease Score":Result['highscore'] ,"Disease Scores":Result['AllDiseasesScores']},200
+        return jsonify({'success':True,'message':"Comparison Done Successfully","Result Disease ": Result['ResultDisease'],
+        "Result Disease Score":Result['HighScore'] ,"Disease Scores":Result['AllDiseasesScores']})
     else:
         return{'success':False, 'err':"Patient doesn't exist in database"},404
 
