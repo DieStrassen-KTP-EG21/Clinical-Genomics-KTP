ea95950a9187bb102301bdd5f79129ddacd78dbb
Etchbanna <albannahisham@gmail.com>
2021-08-26 02:48:07 +0200

    Edited

--- Genome Analyzer/API/GA/models/genbank.py
+++ Genome Analyzer/API/GA/models/genbank.py
@@ -11,34 +11,27 @@ for file in file_names:
     for sq in SeqIO.parse("GA/models/disease_sequences/"+file,"genbank"):
         AllRecs.append(sq)
 
-# sseq=Seq("CGTAGCTAGTGCTAGTCGTAGCAGTCGTGTCAGTCGTACGTATGCTGATGCTAGCGTACGTAGCTAGTGCTAGTCGTAGCAGTCGTGTCAGTCGTACGTATGCTGATGCTAGCGTACGTAGCTAGTGCTAGTCGTAGCAGTCGTGTCAGTCGTACGTATGCTGATGCTAGCGTACGTAGCTAGTGCTAGTCGTAGCAGTCGTGTCAGTCGTACGTATGCTGATGCTAGCGTACGTAGCTAGTGCTAGTCGTAGCAGTCGTGTCAGTCGTACGTATGCTGATGCTAGCGTACGTAGCTAGTGCTAGTCGTAGCAGTCGTGTCAGTCGTACGTATGCTGATGCTAGCGTACGTAGCTAGTGCTAGTCGTAGCAGTCGTGTCAGTCGTACGTATGCTGATGCTAGCGTACGTAGCTAGTGCTAGTCGTAGCAGTCGTGTCAGTCGTACGTATGCTGATGCTAGCGTACGTAGCTAGTGCTAGTCGTAGCAGTCGTGTCAGTCGTACGTATGCTGATGCTAGCGTACGTAGCTAGTGCTAGTCGTAGCAGTCGTGTCAGTCGTACGTATGCTGATGCTAGCGTACGTAGCTAGTGCTAGTCGTAGCAGTCGTGTCAGTCGTACGTATGCTGATGCTAGCGTACGTAGCTAGTGCTAGTCGTAGCAGTCGTGTCAGTCGTACGTATGCTGATGCTAGCGTACGTAGCTAGTGCTAGTCGTAGCAGTCGTGTCAGTCGTACGTATGCTGATGCTAGCGTACGTAGCTAGTGCTAGTCGTAGCAGTCGTGTCAGTCGTACGTATGCTGATGCTAGCGTACGTAGCTAGTGCTAGTCGTAGCAGTCGTGTCAGTCGTACGTATGCTGATGCTAGCGTACGTAGCTAGTGCTAGTCGTAGCAGTCGTGTCAGTCGTACGTATGCTGATGCTAGCGTA")
 
 aligner = Align.PairwiseAligner()
 aligner.match_score=1.0
 
 
-def compare(patientDna,storedData,resultDisease):
+def compare(patientDna,storedData):
     scores=[]
     DiseaseScores={}
     lastm=None
+    resultDisease=None
     for rec in storedData:
         result=pairwise2.align.globalxx(patientDna,rec.seq)
-        # print(rec.description)
-        # print(format_alignment(*result[0], full_sequences=True))
         DiseaseScores[rec.description]=str(aligner.score(patientDna,rec.seq))
         scores.append(aligner.score(patientDna,rec.seq))
-        # print(aligner.score(patientDna,rec.seq))
         m=max(scores)
         if(lastm!=m):
             resultDisease=rec.description
         lastm=m
     return{'ResultDisease':resultDisease, 'HighScore':max(scores), 'Allscores':scores,'AllDiseasesScores':DiseaseScores}
 
-# res=None
-# a=compare(sseq,AllRecs,res)
 
-# print(a['ResultDisease'])
-# print(a['AllDiseasesScores'])
 
 
 
